30866108b14dcad3c60bf091b94624bf95acdadb
sbksba <sbksba@gmail.com>
2016-12-14 18:47:21 +0100

    [MaJ] update the clean script

--- clean.sh
+++ clean.sh
@@ -2,4 +2,4 @@
 #clean.sh
 
 rm -f *~ */*~ */*.pyc
-rm -fr */__pycache__ profile data/Small/The_Matrix.avi data/Small/Lucy.avi tests/tmp/*
+rm -fr */__pycache__ profile data/Small/The_Matrix.avi data/Small/Lucy.avi
