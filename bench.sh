d5348182d4de9bbb864db053e5bed4257920b135
sbksba <sbksba@gmail.com>
2016-12-14 18:34:39 +0100

    [MaJ] update the bench test

--- bench.sh
+++ bench.sh
@@ -4,21 +4,21 @@
 ./clean.sh
 path="data/Small/"
 echo "PRINT"
-python main.py -d $path -n ZERO -p movie
+python main.py -d $path -n zero -p movie
 echo ""
 echo "THEATER"
-python main.py -d $path -n ZERO -a nowPlaying
+python main.py -d $path -n zero -a nowPlaying
 echo ""
 echo "STAT"
-python main.py -d $path -n ZERO -s terminal -t movie
+python main.py -d $path -n zero -s terminal -t movie
 echo ""
 echo "WEB"
-python main.py -d $path -n ZERO -w all -t movie
+python main.py -d $path -n zero -w all -t movie
 touch $path/The_Matrix.avi
 touch $path/Lucy.avi
 echo ""
 echo "UPDATE"
-python main.py -d $path -n ZERO -p movie
+python main.py -d $path -n zero -p movie
 echo ""
 echo "ERASE"
-python main.py -d $path -e ZERO
+python main.py -d $path -e zero
